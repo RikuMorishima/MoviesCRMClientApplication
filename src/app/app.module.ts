1a9967fa76e8953e3dd732cf0a9ab222c7c14fa1
Riku Morishima <riku.morishima@gmail.com>
2022-08-18 19:21:28 -0500

    WIP: adding dynamically changing vertical bar graph

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,23 +12,37 @@ import { CreateCastComponent } from './admin/create-cast/create-cast.component';
 import { FormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin/admin-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ToastrModule } from 'ngx-toastr';
+import { NgProgressModule } from 'ngx-progressbar';
+import { ErrorHandlingInterceptor } from './interceptors/error-handling.interceptor';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpProgressbarInterceptor } from './interceptors/http-progressbar.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesCreateComponent,
-    CreateMovieComponent,
-    CreateCastComponent,
     PageNotFoundComponent,    
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
+    FormsModule,    
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    NgProgressModule,
+    AdminModule,
     UserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS, useClass:ErrorHandlingInterceptor, multi:true
+    }, 
+    {provide: HTTP_INTERCEPTORS, useClass:HttpProgressbarInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
